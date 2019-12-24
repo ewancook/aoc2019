d1b1e5b5c36e5e790ccdea59a211e2aad999e6a2
Ewan <ewan.cook@live.com>
2019-12-24 00:17:46 +0000

    update day 13

--- 13/arcade.py
+++ 13/arcade.py
@@ -8,21 +8,19 @@ m = defaultdict(dict, {})
 code = parse_intcode(input, [])
 for x in code:
     y = next(code)
-    t = ["", "|", "#", "_", "o"][next(code)]
-    m[y][x] = t
+    m[y][x] = ["", "|", "#", "_", "o"][next(code)]
 
 cursor, score, joystick = 0, 0, []
 code = parse_intcode([2]+input[1:], joystick)
 for x in code:
     y = next(code)
     t = next(code)
-    if [x, y] == [-1, 0]:
+    if x == -1 and not y:
         score = t
-    else:
-        if t == 4:
-            joystick.append(0 if x == cursor else -1 if x < cursor else 1)
-        elif t == 3:
-            cursor = x
+    elif t == 4:
+        joystick.append(0 if x == cursor else -1 if x < cursor else 1)
+    elif t == 3:
+        cursor = x
 
 print("Part One:", [t for x in m.values() for t in x.values()].count("#"))
 print("Part Two:", score)
